8d1742e8847c41ec8b72650d54f74b6142f07290
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-03-05 19:54:39 -0500

    Move footer to it's own svelte component and add github link

---
+++ src/Footer.svelte
@@ -0,0 +1,13 @@
+<footer class="self-center">
+  &copy; 2020. Built by
+  <a
+    title="My Github Profile"
+    class="text-blue-600"
+    href="https://github.com/itsrainingmani">
+    <i>@itsrainingmani</i>
+  </a>
+  using
+  <a title="Svelte Website" class="text-red-600" href="https://svelte.dev">
+    <i>Svelte</i>
+  </a>
+</footer>
