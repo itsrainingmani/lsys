c5852fe3fc277ed7fa00ff82a9fdaa7da9e576d5
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-03-06 14:32:30 -0500

    Add graph paper svg background for div

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,8 @@
   let enableTransition = false;
   let turtle_formula = "";
   let ctx = "";
-  let svg_dims = { w: 100, h: 100 };
+  let svg_w = 100; // This is relative to the actual width in pixels
+  let svg_h = 100; // This is relative to the actual height in pixels
   const sample_svgs = [
     [
       "Heart",
@@ -27,13 +28,13 @@
   });
 
   function handleInputChange(event) {
-    console.log(svg_dims);
+    console.log(svg_w, svg_h);
     // Move the svg context to the center
     ctx = path();
-    ctx.moveTo(svg_dims.w / 2, svg_dims.h / 2);
+    ctx.moveTo(svg_w / 2, svg_h / 2);
     let t = turtle_formula[0];
     if (t === "F") {
-      ctx.lineTo(svg_dims.w / 2, svg_dims.h / 2 - 6);
+      ctx.lineTo(svg_w / 2, svg_h / 2 - 6);
     }
   }
 </script>
@@ -53,7 +54,7 @@
   }
 
   svg {
-    stroke-width: 2%;
+    stroke-width: 0.7%;
     stroke-opacity: 90%;
   }
 
@@ -65,6 +66,11 @@
     h1 {
       font-size: 3em;
     }
+  }
+
+  .svg-box {
+    background-color: #f8fcff;
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%23c6ddff' fill-opacity='0.4'%3E%3Cpath opacity='.5' d='M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z'/%3E%3Cpath d='M6 5V0H5v5H0v1h5v94h1V6h94V5H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
   }
 </style>
 
@@ -79,7 +85,7 @@
         type="text"
         class="transition-colors duration-100 ease-in-out bg-white shadow-md
         focus:outline-0 border border-transparent placeholder-gray-600
-        rounded-lg py-2 pr-4 pl-4 block w-full appearance-none leading-normal
+        rounded-lg py-2 px-4 block w-full appearance-none leading-normal
         ds-input text-center"
         bind:value={turtle_formula}
         on:input={handleInputChange}
@@ -89,10 +95,10 @@
     <br />
     <div
       transition:fly={{ y: -50, duration: 500 }}
-      class="flex items-center transition-colors duration-100 ease-in-out
-      bg-white shadow-md focus:outline-0 border border-transparent
-      placeholder-gray-600 rounded-lg py-2 pr-4 pl-4 block w-full
-      appearance-none leading-normal">
+      class="svg-box flex items-center transition-colors duration-100
+      ease-in-out bg-white shadow-md focus:outline-0 border border-transparent
+      placeholder-gray-600 rounded-lg block w-full appearance-none
+      leading-normal">
       <svg viewBox="0 0 100 100">
         <path stroke="blue" fill="none" d={ctx} />
       </svg>
