de6d838a1aef31d151db56ce475353eafbc37ed6
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-03-06 01:05:29 -0500

    Move footer css to Footer.svelte

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,3 +1,17 @@
+<style>
+  footer {
+    text-align: center;
+    font-family: "Rubik", sans-serif;
+    font-size: 1em;
+  }
+
+  @media (max-width: 640px) {
+    footer {
+      font-size: smaller;
+    }
+  }
+</style>
+
 <footer class="self-center">
   &copy; 2020. Built by
   <a
