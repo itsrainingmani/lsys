bd52dd80f269a466e4b4b5afb4c89e6aacbbcf4d
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-03-09 19:12:21 -0400

    Remove unused imports. move turtle svg and code to separate file

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,18 @@
 <script>
   import { onMount } from "svelte";
   import { fly, draw } from "svelte/transition";
-  import { quintOut } from "svelte/easing";
-  import Footer from "./Footer.svelte";
-  import { path } from "d3-path";
-  import { typewriter } from "./utils";
+  // import { quintOut } from "svelte/easing";
+  import Footer from "./Footer";
+  import Turtle from "./Turtle";
 
   let enableTransition = false;
-  let turtle_formula = "";
-  let ctx = "";
-  let svg_w = 100; // This is relative to the actual width in pixels
-  let svg_h = 100; // This is relative to the actual height in pixels
+  let formula = "";
   const sample_svgs = [
     [
       "Heart",
       "M 10,30 A 20,20 0,0,1 50,30 A 20,20 0,0,1 90,30 Q 90,60 50,90 Q 10,60 10,30 z"
     ]
   ];
-  // let [turtle_formula, ctx] = sample_svgs[0];
 
   // When the component is mounted, set enableTransition to true
   // This will trigger the animation
@@ -26,17 +21,6 @@
       enableTransition = true;
     }, 1000);
   });
-
-  function handleInputChange(event) {
-    console.log(svg_w, svg_h);
-    // Move the svg context to the center
-    ctx = path();
-    ctx.moveTo(svg_w / 2, svg_h / 2);
-    let t = turtle_formula[0];
-    if (t === "F") {
-      ctx.lineTo(svg_w / 2, svg_h / 2 - 6);
-    }
-  }
 </script>
 
 <style>
@@ -53,11 +37,6 @@
     line-height: normal;
   }
 
-  svg {
-    stroke-width: 0.7%;
-    stroke-opacity: 90%;
-  }
-
   /* Media Query - this only fires when the condition is met */
   @media (max-width: 640px) {
     p {
@@ -66,11 +45,6 @@
     h1 {
       font-size: 3em;
     }
-  }
-
-  .svg-box {
-    background-color: #f8fcff;
-    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%23c6ddff' fill-opacity='0.4'%3E%3Cpath opacity='.5' d='M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z'/%3E%3Cpath d='M6 5V0H5v5H0v1h5v94h1V6h94V5H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
   }
 </style>
 
@@ -87,21 +61,11 @@
         focus:outline-0 border border-transparent placeholder-gray-600
         rounded-lg py-2 px-4 block w-full appearance-none leading-normal
         ds-input text-center"
-        bind:value={turtle_formula}
-        on:input={handleInputChange}
+        bind:value={formula}
         aria-label="Turtle" />
     </div>
     <br />
-    <div
-      transition:fly={{ y: -50, duration: 500 }}
-      class="svg-box items-center transition-colors duration-100 ease-in-out
-      bg-white shadow-md focus:outline-0 border border-transparent
-      placeholder-gray-600 rounded-lg block w-full appearance-none
-      leading-normal">
-      <svg viewBox="0 0 100 100">
-        <path stroke="blue" fill="none" d={ctx} />
-      </svg>
-    </div>
+    <Turtle {formula} />
   {/if}
 </main>
 <Footer />
---
+++ src/Turtle.svelte
@@ -0,0 +1,52 @@
+<script>
+  import { fly } from "svelte/transition";
+  import { path } from "d3-path";
+  /** Only valid instructions are 'F', 'f', '-', '+'
+   * 'F' means move forward one unit and trace the path with a line.
+   * 'f' means move forward one unit but don't draw anything.
+   * '-' means rotate counter-clockwise but don't move.
+   * '+' means rotate clockwise but don't move.
+   */
+  export let formula;
+  let ctx = "";
+  let svg_w = 100; // This is relative to the actual width in pixels
+  let svg_h = 100; // This is relative to the actual height in pixels
+  const DEG_TO_RAD = Math.PI / 180;
+  /**
+   * SVG Layout -
+   * (0,0) is at the top left corner
+   * (x, y) is at bottom left
+   */
+  function draw_svg(f) {
+    console.log(svg_w, svg_h);
+    // Move the svg context to the center
+    ctx = path();
+    ctx.moveTo(svg_w / 2, svg_h / 2);
+    let t = f[0];
+    if (t === "F") {
+      ctx.lineTo(svg_w / 2, svg_h / 2 - 6);
+    }
+  }
+</script>
+
+<style>
+  svg {
+    stroke-width: 0.7%;
+    stroke-opacity: 90%;
+  }
+
+  .svg-box {
+    background-color: #f8fcff;
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%23c6ddff' fill-opacity='0.4'%3E%3Cpath opacity='.5' d='M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z'/%3E%3Cpath d='M6 5V0H5v5H0v1h5v94h1V6h94V5H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+  }
+</style>
+
+<div
+  transition:fly={{ y: -50, duration: 500 }}
+  class="svg-box items-center transition-colors duration-100 ease-in-out
+  bg-white shadow-md focus:outline-0 border border-transparent
+  placeholder-gray-600 rounded-lg block w-full appearance-none leading-normal">
+  <svg viewBox="0 0 100 100">
+    <path stroke="blue" fill="none" d={ctx} />
+  </svg>
+</div>
