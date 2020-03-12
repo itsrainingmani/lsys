1b09dd7b9d44287852e912e2065ad9df3a38f64e
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-03-11 22:16:06 -0400

    Flip the input range

--- src/App.svelte
+++ src/App.svelte
@@ -101,8 +101,8 @@
       <input
         title="Turtle SVG Scaler"
         type="range"
-        min="0.1"
-        max="1"
+        min="1"
+        max="10"
         step="0.1"
         bind:value={turtleScale} />
     </div>
