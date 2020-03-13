1f1f0e7a84619077ae262c128b380d2f0d04326d
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-03-13 15:02:43 -0400

    Add writable stores for svg stroke color and turn angle

--- src/stores.js
+++ src/stores.js
@@ -2,3 +2,5 @@ import { writable } from "svelte/store";
 
 export const turtleInput = writable("");
 export const turtleIter = writable(1);
+export const svgStrokeColor = writable("#ff3e00");
+export const turnAngle = writable(45);
