9cfe0e24b3ddd211cfca4427664a2b9932b0c95e
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-03-14 03:25:10 -0400

    Create new writable store for svg stroke width

--- src/stores.js
+++ src/stores.js
@@ -4,3 +4,4 @@ export const turtleInput = writable("");
 export const turtleIter = writable(1);
 export const svgStrokeColor = writable("#ff3e00");
 export const turnAngle = writable(45);
+export const svgStrokeWidth = writable(0.2);
