44eabbc3a123598bc2aa7debb9020ffeeef81a9d
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-03-15 22:01:07 -0400

    Remove the margin left for share btn

--- src/Turtle.svelte
+++ src/Turtle.svelte
@@ -248,7 +248,7 @@
 </div>
 <div
   class="inline-flex transition-colors duration-100 ease-in-out bg-white shadow
-  rounded appearance-none leading-tight ml-4 my-1">
+  rounded appearance-none leading-tight ml-4">
   <button
     title="Share Fractal"
     on:click={shareState}
