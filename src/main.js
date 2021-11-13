ccf4ff62dfb30dcea5ec75931d901b54ab091b6d
Manikandan Sundararajan <mani@hey.com>
2021-11-13 00:54:56 -0500

    Fix App.svelte import

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import App from "./App.svelte";
+import App from "./components/App.svelte";
 import "./main.css";
 
 const app = new App({
-	target: document.body
+	target: document.body,
 });
 
 export default app;
