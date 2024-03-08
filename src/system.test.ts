fe616febfd42352db3fc3a87bd1bdc3eb26c9060
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-03-08 17:51:20 -0500

    add some tests using vitest

---
+++ src/system.test.ts
@@ -0,0 +1,28 @@
+import { expect, test } from "vitest";
+import { LSystem } from "./system";
+
+test("multiple symbols", () => {
+	let lsys = new LSystem("F,G;FF;F:F+F+F+F,G:G+G+G+G");
+	// console.log(lsys.rule_map, lsys.symbols);
+	expect(lsys.symbols).toEqual(["F", "G"]);
+});
+
+test("single symbol", () => {
+	let lsys = new LSystem("F;F;F:F+F+F+F");
+	// console.log(lsys.rule_map, lsys.symbols);
+	expect(lsys.symbols).toEqual(["F"]);
+});
+
+test("axiom", () => {
+	let lsys = new LSystem("F;FF;F:F+F+F+F");
+	// console.log(lsys.rule_map, lsys.symbols);
+	expect(lsys.axiom).toEqual("FF");
+});
+
+test("rewrite rule map", () => {
+	let lsys = new LSystem("F;F;F:F+F+F+F");
+	let rewrite_map = new Map<string, string[]>([
+		["F", ["F", "+", "F", "+", "F", "+", "F"]],
+	]);
+	expect(lsys.rule_map).toEqual(rewrite_map);
+});
--- src/system.ts
+++ src/system.ts
@@ -1,40 +1,116 @@
+export const DEG_TO_RAD = Math.PI / 180;
+
 export class LSystem {
-	rule_map: { string: string };
+	rule_map: Map<string, string[]>;
+	symbols: string[];
+	axiom: string;
 
-	constructor(formula: string) {
-		if (!formula.includes(":")) {
-			throw Error("Formula is invalid");
+	// The system is formalized as so ->
+	// Set(Symbols);Start;Production Rules,...
+	// F,G;F;F:F+F+F,G:G+G+G
+	constructor(system: string) {
+		if (!system.includes(":")) {
+			throw new Error("System is invalid");
 		}
+		this.rule_map = new Map();
 
-		formula.split(";").forEach((rule) => {
+		let [symbols, axiom, rewrite_rules] = system.trim().split(";");
+		this.axiom = axiom;
+		let symbol_list = new Set(symbols.replace(" ", "").split(","));
+		this.symbols = [...symbol_list.keys()];
+
+		rewrite_rules.split(",").forEach((rule) => {
 			let f = rule.split(":");
 			if (f.length > 2) {
-				throw Error("Invalid Rule");
+				throw new Error("Invalid Rule");
 			}
 
 			let [symbol, rewrite_rule] = f;
-			this.rule_map[symbol] = rewrite_rule;
+			if (this.symbols.includes(symbol)) {
+				this.rule_map.set(symbol, rewrite_rule.split(""));
+			}
 		});
 	}
 
-	rewrite(num_iters: number) {}
+	rewrite(num_iters: number): string[] {
+		// Start off with the axiom | start condition
+		let transformed = this.axiom.split("");
+		for (let i = 0; i < num_iters; i++) {
+			// Apply transformation
+			let substituted = transformed.flatMap((t) => {
+				return this.rule_map.get(t) ?? [t];
+			});
+		}
+
+		return transformed;
+	}
 }
 
-class Turtle {
+export class DrawingContext {
 	context: CanvasRenderingContext2D;
-	x: number;
-	y: number;
-	system: LSystem;
+	width: number;
+	height: number;
 
 	constructor(
 		context: CanvasRenderingContext2D,
-		x: number,
-		y: number,
-		system: LSystem
+		strokeColor = "blue",
+		strokeWidth = 10
 	) {
+		this.context = context;
+		this.context.strokeStyle = strokeColor;
+		this.context.lineWidth = strokeWidth;
+		this.width = this.context.canvas.width;
+		this.height = this.context.canvas.height;
+
+		this.context.clearRect(0, 0, this.width, this.height);
+		this.context.imageSmoothingEnabled = false;
+	}
+
+	updateColor(strokeColor: string) {
+		this.context.strokeStyle = strokeColor;
+	}
+
+	updateWidth(strokeWidth: number) {
+		this.context.lineWidth = strokeWidth;
+	}
+
+	setup() {}
+	scaleBy(scale_factor: number) {}
+	pan(factor: number) {}
+}
+
+export class Turtle {
+	context: DrawingContext;
+	x: number;
+	y: number;
+	moveAmt: number = 10;
+	turnAngle: number;
+
+	constructor(context: DrawingContext, x: number, y: number, turnAngle = 45) {
 		this.context = context;
 		this.x = x;
 		this.y = y;
-		this.system = system;
+		this.turnAngle = turnAngle;
+	}
+
+	moveForward(by?: number) {
+		console.log(this.context.context);
+		let end_x =
+			this.x + Math.sin(this.turnAngle * DEG_TO_RAD) * (by ?? this.moveAmt);
+		let end_y =
+			this.y + Math.cos(this.turnAngle * DEG_TO_RAD) * (by ?? this.moveAmt);
+		// let [end_x, end_y] = [100, 100];
+		this.context.context.beginPath();
+		this.context.context.moveTo(this.x, this.y);
+		this.context.context.lineTo(end_x, end_y);
+		this.context.context.stroke();
+		this.context.context.closePath();
+		this.x = end_x;
+		this.y = end_y;
+		// this.context.context.fillRect(this.x, this.y, 100, 100);
+	}
+
+	turn(by: number) {
+		this.turnAngle += by;
 	}
 }
